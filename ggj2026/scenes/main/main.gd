559a3469568bb6f63eb1d7bde5ea3f0e3e20ec75
Elsa Weber <elsa.weber0@gmail.com>
2026-01-30 20:58:37 +0100

    light signal

--- ggj2026/scenes/main/main.gd
+++ ggj2026/scenes/main/main.gd
@@ -1,5 +1,7 @@
 extends Node
 
+signal switch_light(on)
+
 # TODO: less hard-coded ?
 var 	WIDTH = 1920
 var HEIGHT = 1080
@@ -7,12 +9,19 @@ var HEIGHT = 1080
 var TRANS_TIME = 0.5
 var CAMERA_UP = Vector2(WIDTH*0.5, HEIGHT*0.5)
 var CAMERA_DOWN = Vector2(WIDTH*0.5, HEIGHT*1.5)
+var CAMERA_POS = "up"
 
 func _input(event):
-	if event.is_action_pressed("ui_down"):
-		camera_trans(CAMERA_DOWN)
-	elif event.is_action_pressed("ui_up"):
-		camera_trans(CAMERA_UP)
+	if CAMERA_POS == "up":
+		if event.is_action_pressed("ui_down"):
+			camera_trans(CAMERA_DOWN)
+			CAMERA_POS = "down"
+	else:
+		if event.is_action_pressed("ui_up"):
+			camera_trans(CAMERA_UP)
+			CAMERA_POS = "up"
+		elif event.is_action_pressed("activate"):
+			switch_light.emit(true)
 		
 func camera_trans(new_pos):
 	create_tween().set_trans(Tween.TRANS_SINE).tween_property($Camera, "position", new_pos, TRANS_TIME)
