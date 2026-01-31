98e62b7c0f4b956a3bbc6c73c337bbbb9ac0e033
Elsa Weber <elsa.weber0@gmail.com>
2026-01-31 13:08:31 +0100

    change type of type

--- ggj2026/mask.gd
+++ ggj2026/mask.gd
@@ -1,19 +1,15 @@
 extends Node2D
 
-@export var type: String
+@export var type: int
 
 signal mask_on(type)
 
 func _ready():
-	%Sprite.texture = load("res://scenes/main/assets/mask_%s_wood.png" % type)
+	%Sprite.texture = load("res://scenes/main/assets/mask_%d_wood.png" % type)
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
 		%Anim.play("put_on_mask")
-
-
-func _on_anim_animation_finished(anim_name: StringName) -> void:
-	if anim_name == "put_on_mask":
 		mask_on.emit(type)
 
 func remove():
