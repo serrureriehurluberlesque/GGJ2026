05ad254f89212918d0e997c472aca4a7594ec5e1
Hugo Dupraz <hugo.dupraz@gmail.com>
2026-01-31 22:25:33 +0100

    update assets menu and cie

--- ggj2026/scenes/credits/credits.gd
+++ ggj2026/scenes/credits/credits.gd
@@ -2,9 +2,16 @@ extends TextureRect
 
 var t = 0
 
+var credits2 = false
+
 func _input(event):
-	if event.is_pressed() and t >= 1.0:
+	if event.is_pressed() and t >= 1.0 and credits2:
 		get_tree().change_scene_to_file("res://scenes/main/main.tscn")
+	if event.is_pressed() and t >= 1.0 and not credits2:
+		$Credits2.show()
+		credits2 = true
+		t = 0
+	
 
 func _process(delta: float) -> void:
 	t += delta
