65390fa2a08780c40f5596db8b42f07f6ec3e63c
Hugo Dupraz <hugo.dupraz@gmail.com>
2026-01-31 11:16:06 +0100

    better spawn

--- ggj2026/scenes/monster/monster.gd
+++ ggj2026/scenes/monster/monster.gd
@@ -1,4 +1,40 @@
+class_name Monster
 extends Node2D
 
+const monster_scene: PackedScene = preload("res://scenes/monster/monster.tscn")
+
+var type: int
+var side: String
+var steps: Array
+var step: int
+
+var params_sides = {
+	"left": {"pos": Vector2(480, 300), "init_function": set_mask_left}, 
+	"center": {"pos": Vector2(860, 300), "init_function": set_mask_center}, 
+	"right": {"pos": Vector2(1340, 300), "init_function": set_mask_right},
+}
+
+static func new_monster(type: int, side: String, total_time: float) -> Monster:
+	var new_monster: Monster = monster_scene.instantiate()
+	new_monster.type = type
+	new_monster.side = side
+	new_monster.steps = []
+	new_monster.step = 0
+	return new_monster
+
 func _ready():
-	print("Raaarg, i'm a monster")
+	print("Raaarg, i'm a monster at %s" % get_global_position())
+	params_sides[side]["init_function"].call()
+	
+	set_global_position(params_sides[side]["pos"])
+	set_scale(Vector2(0.25, 0.25))
+
+func set_mask_left():
+	$Sprite2D.texture = load("res://scenes/monster/assets/mask_%d_side.png" % type)
+	
+func set_mask_center():
+	$Sprite2D.texture = load("res://scenes/monster/assets/mask_%d_front.png" % type)
+	
+func set_mask_right():
+	$Sprite2D.texture = load("res://scenes/monster/assets/mask_%d_side.png" % type)
+	$Sprite2D.flip_h = true
--- ggj2026/scenes/monsters/monsters.gd
+++ ggj2026/scenes/monsters/monsters.gd
@@ -7,15 +7,15 @@ var BASE_TIME_MONSTER_SPAWN = 5.0
 var TIME_MONSTER_RANDF = 10.0
 var TIME_MONSTER_COEF = 1.0
 
+var SPEED_MONSTER = 10.0
+
 var MIN_NBR_MONSTERS = 6
 var MAX_NBR_MONSTERS = 9
 
-var SPAWN_Y = 400
-
 var spawn_sides = {
-	"left": {"pos": Vector2(200, 200), "monsters": [], "mask": set_mask_left}, 
-	"center": {"pos": Vector2(400, 200), "monsters": [], "mask": set_mask_center}, 
-	"right": {"pos": Vector2(600, 200), "monsters": [], "mask": set_mask_right},
+	"left": {"monsters": []}, 
+	"center": {"monsters": []}, 
+	"right": {"monsters": []},
 }
 
 var next_monsters = []
@@ -31,15 +31,6 @@ func _ready():
 	
 	next_monster()
 
-func set_mask_left(monster):
-	monster.get_node("Sprite2D").texture = load("res://scenes/monster/assets/mask_1_front.png")
-	
-func set_mask_center(monster):
-	monster.get_node("Sprite2D").texture = load("res://scenes/monster/assets/mask_1_front.png")
-	
-func set_mask_right(monster):
-	monster.get_node("Sprite2D").texture = load("res://scenes/monster/assets/mask_1_front.png")
-
 func next_monster():
 	if next_monsters:
 		$TimerNextMonster.start(next_monsters.pop_front())
@@ -47,19 +38,18 @@ func next_monster():
 		win.emit()
 
 func spawn_monster():
-	var new_monster = monster.instantiate()
 	var empty_side = []
 	for s in spawn_sides.keys():
 		if not spawn_sides[s]["monsters"]:
 			empty_side.append(s)
-	var side = empty_side.pick_random()
 	
 	if empty_side:
+		var side = empty_side.pick_random()
+		var type = randi_range(1, 3)
+		var total_time = SPEED_MONSTER
+		var new_monster = Monster.new_monster(type, side, total_time)
 		add_child(new_monster)
 		spawn_sides[side]["monsters"].append(new_monster)
-		new_monster.set_position(spawn_sides[side]["pos"])
-		new_monster.set_scale(Vector2(0.25, 0.25))
-		spawn_sides[side]["mask"].call(new_monster)
 		
 		next_monster()
 	else:
