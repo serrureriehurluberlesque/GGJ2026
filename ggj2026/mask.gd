129d3152b342bd2554cabbd872cdaa5c4c86a114
Elsa Weber <elsa.weber0@gmail.com>
2026-01-31 10:40:43 +0100

    mask slide

--- ggj2026/mask.gd
+++ ggj2026/mask.gd
@@ -1,6 +1,11 @@
-extends Sprite2D
+extends Node2D
 
+@export var type: String
+
+func _ready():
+	$Sprite2D.texture = load("res://scenes/main/assets/mask_%s_wood.png" % type)
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event.is_action_pressed("click"):
-		self.visible = false
+		print("click")
+		%anim.play("slide_off_table")
