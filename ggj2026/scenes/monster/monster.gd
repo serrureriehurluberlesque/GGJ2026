ed652e9b2f18d0baccaed44c80499d0365e67832
Hugo Dupraz <hugo.dupraz@gmail.com>
2026-01-31 11:54:52 +0100

    better spawn and add signal and fucntions

--- ggj2026/scenes/monster/monster.gd
+++ ggj2026/scenes/monster/monster.gd
@@ -5,8 +5,14 @@ const monster_scene: PackedScene = preload("res://scenes/monster/monster.tscn")
 
 var type: int
 var side: String
-var steps: Array
-var step: int
+var knsea: bool
+var mask: int
+var debug: bool
+
+var steps:= []
+var progress:= 0.0
+var progress_flee:= 0.0
+var step:= 0
 
 var params_sides = {
 	"left": {"pos": Vector2(480, 300), "init_function": set_mask_left}, 
@@ -14,27 +20,44 @@ var params_sides = {
 	"right": {"pos": Vector2(1340, 300), "init_function": set_mask_right},
 }
 
-static func new_monster(type: int, side: String, total_time: float) -> Monster:
+static func new_monster(type: int, side: String, total_time: float, knsea: bool, mask: int, debug: bool) -> Monster:
 	var new_monster: Monster = monster_scene.instantiate()
 	new_monster.type = type
 	new_monster.side = side
-	new_monster.steps = []
-	new_monster.step = 0
+	new_monster.knsea = knsea
+	new_monster.mask = mask
+	new_monster.debug = debug
 	return new_monster
 
 func _ready():
-	print("Raaarg, i'm a monster at %s" % get_global_position())
 	params_sides[side]["init_function"].call()
 	
 	set_global_position(params_sides[side]["pos"])
 	set_scale(Vector2(0.25, 0.25))
+	
+	if debug:
+		print("Raaarg, i'm a monster at %s" % get_global_position())
 
 func set_mask_left():
 	$Sprite2D.texture = load("res://scenes/monster/assets/mask_%d_side.png" % type)
-	
+
 func set_mask_center():
 	$Sprite2D.texture = load("res://scenes/monster/assets/mask_%d_front.png" % type)
-	
+
 func set_mask_right():
 	$Sprite2D.texture = load("res://scenes/monster/assets/mask_%d_side.png" % type)
 	$Sprite2D.flip_h = true
+
+func _physics_process(delta):
+	if not knsea:
+		if mask == type:
+			progress_flee += delta
+		else:
+			progress += delta
+			set_global_position(params_sides[side]["pos"] + Vector2(0, progress * 10.0))
+
+func update_visible(k):
+	knsea = k
+
+func update_masks(m):
+	mask = m
--- ggj2026/scenes/monsters/monsters.gd
+++ ggj2026/scenes/monsters/monsters.gd
@@ -12,15 +12,16 @@ var SPEED_MONSTER = 10.0
 var MIN_NBR_MONSTERS = 6
 var MAX_NBR_MONSTERS = 9
 
-var spawn_sides = {
-	"left": {"monsters": []}, 
-	"center": {"monsters": []}, 
-	"right": {"monsters": []},
-}
+var spawn_sides = [	"left", "center", "right"]
 
 var next_monsters = []
 
 var monster
+
+var knsea:= true
+var mask:= 0
+
+var debug = false
 
 func _ready():
 	monster = load("res://scenes/monster/monster.tscn")
@@ -29,8 +30,13 @@ func _ready():
 	for i in randi_range(MIN_NBR_MONSTERS, MAX_NBR_MONSTERS):
 		next_monsters.append(BASE_TIME_MONSTER_SPAWN + randf() * (TIME_MONSTER_RANDF - TIME_MONSTER_COEF * i))
 	
-	next_monster()
+	if get_tree().current_scene == self:  # si c'est pas la main scene, faudra call start() pour que ça commence
+		start()
+		debug = true
 
+func start():
+	next_monster()
+	
 func next_monster():
 	if next_monsters:
 		$TimerNextMonster.start(next_monsters.pop_front())
@@ -39,18 +45,41 @@ func next_monster():
 
 func spawn_monster():
 	var empty_side = []
-	for s in spawn_sides.keys():
-		if not spawn_sides[s]["monsters"]:
+	for s in spawn_sides:
+		var is_empty = true
+		for m in $Monsters.get_children():
+			if m.side == s:
+				is_empty = false
+				break
+		if is_empty:
 			empty_side.append(s)
 	
 	if empty_side:
 		var side = empty_side.pick_random()
 		var type = randi_range(1, 3)
 		var total_time = SPEED_MONSTER
-		var new_monster = Monster.new_monster(type, side, total_time)
-		add_child(new_monster)
-		spawn_sides[side]["monsters"].append(new_monster)
+		var new_monster = Monster.new_monster(type, side, total_time, knsea, mask, debug)
+		$Monsters.add_child(new_monster)
 		
 		next_monster()
 	else:
 		$TimerNextMonster.start(1.0)
+
+func update_visible(k):
+	knsea = k
+	transmit_knseamask_monsters()
+
+func update_masks(m):
+	mask = m
+	transmit_knseamask_monsters()
+
+func transmit_knseamask_monsters():
+	for m in $Monsters.get_children():
+		m.update_knsea(knsea)
+		m.update_masks(mask)
+
+func monster_flee(m):
+	m.free()
+	
+	if not next_monster() and not $Monsters.get_children():
+		win.emit()
