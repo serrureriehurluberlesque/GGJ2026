a2429ae812d799aafb55c98be88f4e7421cd6e7f
Hugo Dupraz <hugo.dupraz@gmail.com>
2026-01-31 23:13:39 +0100

    update la darkness des monstres

--- ggj2026/scenes/monster/monster.gd
+++ ggj2026/scenes/monster/monster.gd
@@ -30,37 +30,37 @@ var properties_from_step = {
 		"center": [Vector2(1060, 790)],
 		"right": [Vector2(1660, 690)],
 		}},
-	2: {"scale": 0.15, "alpha": 1.0, "dark": 0.00, "poss": {
+	2: {"scale": 0.15, "alpha": 1.0, "dark": 0.05, "poss": {
 		"left": [Vector2(380, 775)], 
 		"center": [Vector2(915, 795)],
 		"right": [Vector2(1790, 770)],
 		}},
-	3: {"scale": 0.19, "alpha": 1.0, "dark": 0.05, "poss": {
+	3: {"scale": 0.19, "alpha": 1.0, "dark": 0.15, "poss": {
 		"left": [Vector2(380, 775)], 
 		"center": [Vector2(915, 795)],
 		"right": [Vector2(1790, 770)],
 		}},
-	4: {"scale": 0.25, "alpha": 1.0, "dark": 0.1, "poss": {
+	4: {"scale": 0.25, "alpha": 1.0, "dark": 0.25, "poss": {
 		"left": [Vector2(380, 905)], 
 		"center": [Vector2(975, 1010)],
 		"right": [Vector2(1425, 965)],
 		}},
-	5: {"scale": 0.31, "alpha": 1.0, "dark": 0.15, "poss": {
+	5: {"scale": 0.31, "alpha": 1.0, "dark": 0.35, "poss": {
 		"left": [Vector2(380, 905)], 
 		"center": [Vector2(975, 1010)],
 		"right": [Vector2(1425, 965)],
 		}},
-	6: {"scale": 0.39, "alpha": 1.0, "dark": 0.25, "poss": {
+	6: {"scale": 0.39, "alpha": 1.0, "dark": 0.45, "poss": {
 		"left": [Vector2(430, 1210)], 
 		"center": [Vector2(1120, 1220)],
 		"right": [Vector2(1730, 1130)],
 		}},
-	7: {"scale": 0.5, "alpha": 1.0, "dark": 0.35, "poss": {
+	7: {"scale": 0.5, "alpha": 1.0, "dark": 0.55, "poss": {
 		"left": [Vector2(430, 1210)], 
 		"center": [Vector2(1120, 1220)],
 		"right": [Vector2(1730, 1130)],
 		}},
-	8: {"scale": 0.62, "alpha": 1.0, "dark": 0.5, "poss": {
+	8: {"scale": 0.62, "alpha": 1.0, "dark": 0.65, "poss": {
 		"left": [Vector2(430, 1210)], 
 		"center": [Vector2(1120, 1220)],
 		"right": [Vector2(1730, 1130)],
@@ -104,10 +104,6 @@ func set_mask_texture():
 
 func _physics_process(delta):
 	if not knsea:
-		#if debug and visible:
-			#print("on me %s voit plus" % name)
-		if not debug:
-			hide()
 		if mask == type:
 			progress_flee = min(1.0, progress_flee + delta * speed * 3.0)
 			if progress_flee >= 1.0:
@@ -120,19 +116,18 @@ func _physics_process(delta):
 			elif progress > steps[step]:
 				step += 1
 				update_progress_pos()
-	else:
-		#if debug and not visible:
-			#print("on me %s voit" % name)
-		show()
 
 func update_progress_pos():
 	set_global_position(properties_from_step[step]["poss"][side][0])
 	set_scale(Vector2(properties_from_step[step]["scale"], properties_from_step[step]["scale"]))
 	z_index = step
+	var dark_factor = 1.0
+	if not knsea:
+		dark_factor = 0.05
 	modulate = Color(
-		properties_from_step[step]["dark"] * 1.0, 
-		properties_from_step[step]["dark"] * 1.0, 
-		properties_from_step[step]["dark"] * 1.0,
+		properties_from_step[step]["dark"] * 1.0 * dark_factor, 
+		properties_from_step[step]["dark"] * 1.0 * dark_factor, 
+		properties_from_step[step]["dark"] * 1.0 * dark_factor,
 		properties_from_step[step]["alpha"] * 1.0
 		)
 	if debug:
@@ -145,6 +140,7 @@ func update_progress_pos():
 
 func update_knsea(k):
 	knsea = k
+	update_progress_pos()
 
 func update_masks(m):
 	mask = m
