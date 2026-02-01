77b99b066847bc9ed50dc006c38c76ca5ba4b5d1
Elsa Weber <elsa.weber0@gmail.com>
2026-02-01 16:15:15 +0100

    better choice

--- ggj2026/scenes/main/choice.gd
+++ ggj2026/scenes/main/choice.gd
@@ -2,8 +2,12 @@ extends TextureRect
 
 func _on_yes_pressed() -> void:
 	Global.gentil = false
+	$StartSound.play()
+	await $StartSound.finished
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
 
 func _on_no_pressed() -> void:
 	Global.gentil = true
+	$StartSound.play()
+	await $StartSound.finished
 	get_tree().change_scene_to_file("res://scenes/main/main.tscn")
